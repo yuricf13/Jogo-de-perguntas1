3cfdf81c72eeb8d86cd480afd4888ad9dd6176da
yuricf13 <yuri.correa.figueiredo@escola.pr.gov.br>
2025-11-10 09:41:06 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -1,8 +1,40 @@
 #Descrição
 print("Jogo das palvaras")
 #Perguntas
-print("Qual o seu nome?")
-nome = input(print("Qual seu nome?")
-print("Qual o seu nome?")
-#Jogo das perguntas
-
+nome = input(print("Qual seu nome?"))
+resposta_correta = "12"
+resposta_pessoa = input(print("Certo" ,nome ,", quantos meses tem 28 dias?"))
+if resposta_pessoa.strip().lower() in resposta_correta.strip().lower():
+    print("✅ Você acertou! Muito bem!")
+else: 
+    print("❌ Você errou. Sinto muito😥")
+print("Próxima!")
+resposta_correta = "23"
+resposta_pessoa = input(print("Quantos anos tinha o Neymar em 2015?"))
+if resposta_pessoa.strip().lower() in resposta_correta.strip().lower():
+    print("✅ Você acertou! Muito bem!")
+else: 
+    print("❌ Você errou. Sinto muito😥")
+print("Próxima!")
+resposta_correta = "Python"
+resposta_pessoa = input(print("Qual o nome da Linguagem que pode ser denominada por py?"))
+if resposta_pessoa.strip().lower() in resposta_correta.strip().lower():
+    print("✅ Você acertou! Muito bem!")
+else: 
+    print("❌ Você errou. Sinto muito😥")
+print("Próxima!")
+resposta_correta = "Mr Beast"
+resposta_pessoa = input(print("Qual o nome do youtuber que atualmente tem mais de 400 milhões de inscritos?"))
+if resposta_pessoa.strip().lower() in resposta_correta.strip().lower():
+    print("✅ Você acertou! Muito bem!")
+else: 
+    print("❌ Você errou. Sinto muito😥")
+print("Próxima!")
+resposta_correta = "1939"
+resposta_pessoa = input(print("Qual o ano que começou a Segunda Guerra Mundial?"))
+if resposta_pessoa.strip().lower() in resposta_correta.strip().lower():
+    print("✅ Você acertou! Muito bem!")
+else: 
+    print("❌ Você errou. Sinto muito😥")
+print("Próxima!")
+print("Muito obrigado por responder😄")
